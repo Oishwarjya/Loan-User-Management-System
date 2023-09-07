355ee59f4269e29d6d89f0ffeee94a11d65b43c8
Subhash Raja <Subhash1.raja@gmail.com>
2023-09-07 11:32:11 +0530

    Changed LamaApplication.java

--- backend/lama/src/main/java/com/capstone/lama/LamaApplication.java
+++ backend/lama/src/main/java/com/capstone/lama/LamaApplication.java
@@ -2,8 +2,9 @@ package com.capstone.lama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class LamaApplication {
 
 	public static void main(String[] args) {
