fb9eea55ca1fa8b01ad8f0395cad40cd67519558
Subhash Raja <Subhash1.raja@gmail.com>
2023-09-15 12:31:36 +0530

    Made changes to Employee entity functionality

--- backend/src/main/java/com/capstone/backend/controllers/EmployeeController.java
+++ backend/src/main/java/com/capstone/backend/controllers/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -44,9 +45,9 @@ public class EmployeeController {
         return employeeService.addEmp(emp);
     }
 
-    @PostMapping("/employees")
+    @GetMapping("/employees")
     @ResponseBody
-    public List<Employee> getAllEmp() throws TableEmptyException
+    public Map<String,Object> getAllEmp() throws TableEmptyException
     {
         return employeeService.getAllEmp();
     }
