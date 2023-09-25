11c7d3975fe1832e44da2e9ce665e2420cf4ebc1
Atishay Patni <patni.atishay02@gmail.com>
2023-09-25 15:31:42 +0530

    Changed the code for user delete functionality a little and added the delete user endpoint and also added a ItemsServiceTests test file for JUnit

---
+++ backend/src/test/java/com/capstone/backend/ItemsServiceTests.java
@@ -0,0 +1,12 @@
+package com.capstone.backend;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ItemsServiceTests {
+    @Test
+    public void demoTest() {
+        assertTrue(true);
+    }
+}
